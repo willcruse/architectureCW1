8d001db9ee3f61bffee7907d3790717aa6de644a
Will Cruse <willcruse123@gmail.com>
2018-11-01 17:21:02 +0000

    Three player

--- exampleCWCode/exampleCWCode.ino
+++ exampleCWCode/exampleCWCode.ino
@@ -1,15 +1,17 @@
 #include <Servo.h>
 
 // assign LEDs, buttons and servos to pins
-Servo player1Servo;
+Servo servo;
 int playerOneButton = 2;
 int playerTwoButton = 3;
+int playerThreeButton = 12;
 int data = 10;
 int clock = 9;
 int latch = 8;
 int resistorPin = A0;
 int speakerPin = 13;
-
+int player3LED[] = {4, 5, 6, 7};
+ 
 // declare variables
 /*
  * Values arrays correspond to the base 10 value of 
@@ -18,13 +20,14 @@ int speakerPin = 13;
 int valuesPlayer1[] = {1, 2, 4};
 int valuesPlayer2[] = {16, 32, 64};
 //playerRandoms contains the random numbers for each player
-int playerRandoms[2];
+int playerRandoms[3];
 /*shiftOutValue is the sum of the player values getting written
  * to the shift register
  */
 int shiftOutValue;
 int scorePlayer1 = 0;
 int scorePlayer2 = 0;
+int scorePlayer3 = 0;
 //Input from the potentiometer
 int sensorValue = 0;
 /*Used to check if LEDs are on or off
@@ -39,37 +42,64 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(2), playerOneInput, FALLING); 
   attachInterrupt(digitalPinToInterrupt(3), playerTwoInput, FALLING);
   //Attach servos and write to starting postion
-  player1Servo.attach(7);
-  player1Servo.write(0);
+  servo.attach(11);
+  servo.write(0);
   //Set pin modes
   pinMode(playerOneButton, INPUT);
   pinMode(playerTwoButton, INPUT);
+  pinMode(playerThreeButton, INPUT);
   pinMode(data, OUTPUT);
   pinMode(clock, OUTPUT);
   pinMode(latch, OUTPUT); 
+  for (int i = 0; i < 4; i++) {
+    pinMode(player3LED[i], OUTPUT);
+  }
   Serial.begin(9600);
 }
 
 //run main program loop
 void loop() {
+  //Serial.println(digitalRead(playerThreeButton));
+  Serial.println(scorePlayer3);
+  if (digitalRead(playerThreeButton) == LOW) {
+    tone(speakerPin, 131, 250);
+  }
   //Turn all LEDs off
   shiftWrite(0);
   //Assign a random number for each player
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 3; i++) {
     playerRandoms[i] = random(3);
   }
   //Sum the random values and write to shift reg
   shiftOutValue = valuesPlayer1[playerRandoms[0]] + valuesPlayer2[playerRandoms[1]];
   shiftWrite(shiftOutValue);
-  //Leaves LEDs on for time determined by potentiometer
-  delay(delayTime());
+  digitalWrite(player3LED[playerRandoms[2]], HIGH);
+  /*Leaves LEDs on for time determined by potentiometer
+   * previous is set to the time the loop is started
+   * current is updated each iteration until there is a difference >= delayTime
+   */
+  unsigned long previousMS = millis();
+  unsigned long currentMS = millis();
+  int playerThreeFlag = 0;
+  while (currentMS - previousMS < delayTime()) {
+    //Check if playerThree has pushed a button
+    if (digitalRead(playerThreeButton) == LOW && playerThreeFlag == 0) {
+      scorePlayer3++;
+      tone(speakerPin, 220, 250);
+      digitalWrite(player3LED[3], HIGH);
+      playerThreeFlag = 1;
+    }
+    currentMS = millis();
+  }
+  //Set all LEDs to LOW
+  digitalWrite(player3LED[3], LOW);
   shiftWrite(0);
+  digitalWrite(player3LED[playerRandoms[2]], LOW);
   delay(delayTime());
-  //Check to see if either player has won by having a score of 10 or more
+  //Check to see if any player has won by having a score of 10 or more
   if (scorePlayer1 >= 10) {
       for (int i = 0; i < 5; i++) {
         shiftWrite(15);
-        tone(speakerPin, 262);
         delay(250);
         shiftWrite(0);
         delay(250);
@@ -79,12 +109,31 @@ void loop() {
     if (scorePlayer2 >= 10) {
         for (int i = 0; i < 5; i++) {
           shiftWrite(240);
-          tone(speakerPin, 262);
           delay(250);
           shiftWrite(0);
           delay(250);
       }
         resetGame();
+    }
+    if (scorePlayer3 >= 10) {
+      for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+          digitalWrite(player3LED[j], HIGH);
+        }
+        delay(250);
+        for (int j = 0; j < 4; j++) {
+          digitalWrite(player3LED[j], LOW);
+        }
+        delay(250);
+        resetGame();
+      }
+    }
+    if (scorePlayer1 > scorePlayer2 && scorePlayer1 > scorePlayer3) {
+      servo.write(0);
+    } else if (scorePlayer2 > scorePlayer3) {
+      servo.write(90);
+    } else {
+      servo.write(180);
     }
 }
 
@@ -94,10 +143,9 @@ void playerOneInput() {
   if (state) {
     shiftWrite(8);
     scorePlayer1++;
-    moveServo(scorePlayer1, player1Servo);
     tone(speakerPin, 262, 250);
   } else {
-    tone(speakerPin, 175, 250);
+    tone(speakerPin, 165, 250);
   }
 }
 
@@ -106,23 +154,18 @@ void playerTwoInput() {
   if (state) {
     shiftWrite(128);
     scorePlayer2++;
-    tone(speakerPin, 131, 250);
+    tone(speakerPin, 247, 250);
   } else {
-    tone(speakerPin, 87, 250);
+    tone(speakerPin, 147, 250);
   }
-}
-
-//Moves a given servo to 18 * the given score degrees
-void moveServo(int score, Servo servo) {
-  float pos = score * 18;
-  servo.write(pos);
 }
 
 //Sets scores to 0 and resets servos
 void resetGame() {
   scorePlayer1 = 0;
   scorePlayer2 = 0;
-  player1Servo.write(0);
+  scorePlayer3 = 0;
+  servo.write(0);
 }
 
 //Function to get input from the potentiometer and then translates that into a delay time
